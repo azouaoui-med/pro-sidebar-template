c71c91888c7c4ad3e6c5177a36cc9ff4134a35f4
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2018-02-25 20:06:50 +0100

    update to bootstrap 4

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 Responsive sidebar template width dropdown menu based on bootstrap framwork
 
 ## Demo
-[Live preview](https://azouaoui-med.github.io/pro-sidebar-template/)
+[Demo with bootstrap 3](https://azouaoui-med.github.io/pro-sidebar-template/bootstrap3)
+[Demo with bootstrap 4](https://azouaoui-med.github.io/pro-sidebar-template/bootstrap4)
 
+## Screenshot
 ![Screenshot](https://user-images.githubusercontent.com/25878302/34446998-7d568970-ece8-11e7-9ba5-deb3c03e72b6.PNG)
 
-## Dependencies
-*   [Bootstrap 3](https://getbootstrap.com/)
+## Resources
+*   [Bootstrap](https://getbootstrap.com/)
 *   [JQuery](http://jquery.com/)
 *   [Font awsome](http://fontawesome.io/)
 *   [Malihu custom scrollbar plugin](https://github.com/malihu/malihu-custom-scrollbar-plugin)
---
+++ bootstrap4/assets/js/custom.js
@@ -0,0 +1,34 @@
+jQuery(function ($) {
+
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if ($(this).parent().hasClass("active")) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).parent().removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).next(".sidebar-submenu").slideDown(200);
+            $(this).parent().addClass("active");
+        }
+
+    });
+
+    $("#toggle-sidebar").click(function () {
+        $(".page-wrapper").toggleClass("toggled");
+    });
+    var themes = "chiller-theme ice-theme cool-theme light-theme green-theme spicy-theme purple-theme";
+    $('[data-theme]').click(function () {
+        $('.page-wrapper').removeClass(themes);
+        $('.page-wrapper').addClass($(this).attr('data-theme'));
+    });
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(".sidebar-content").mCustomScrollbar({
+            axis: "y",
+            autoHideScrollbar: true,
+            scrollInertia: 300
+        });
+        $(".sidebar-content").addClass("desktop");
+
+    }
+});
