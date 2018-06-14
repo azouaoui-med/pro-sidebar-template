dd782f082dded5b0c76fb83eb2da3c0de4d961f9
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2018-06-14 18:49:11 +0200

    Add background image

--- bootstrap4/assets/js/custom.js
+++ bootstrap4/assets/js/custom.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
 
+    // Dropdown menu
     $(".sidebar-dropdown > a").click(function () {
         $(".sidebar-submenu").slideUp(200);
         if ($(this).parent().hasClass("active")) {
@@ -13,18 +14,41 @@ jQuery(function ($) {
 
     });
 
+    // close sidebar 
     $("#close-sidebar").click(function () {
         $(".page-wrapper").removeClass("toggled");
     });
+
+    //show sidebar
     $("#show-sidebar").click(function () {
         $(".page-wrapper").addClass("toggled");
     });
-    var themes = "chiller-theme ice-theme cool-theme light-theme green-theme spicy-theme purple-theme";
+
+    //switch between themes 
+    var themes = "chiller-theme ice-theme cool-theme light-theme";
     $('[data-theme]').click(function () {
+        $('[data-theme]').removeClass("selected");
+        $(this).addClass("selected");
         $('.page-wrapper').removeClass(themes);
         $('.page-wrapper').addClass($(this).attr('data-theme'));
     });
 
+    // switch between background images
+    var bgs = "bg1 bg2 bg3 bg4";
+    $('[data-bg]').click(function () {
+        $('[data-bg]').removeClass("selected");
+        $(this).addClass("selected");
+        $('.page-wrapper').removeClass(bgs);
+        $('.page-wrapper').addClass($(this).attr('data-bg'));
+    });
+
+    // toggle background image
+    $("#toggle-bg").change(function (e) { 
+        e.preventDefault();
+        $('.page-wrapper').toggleClass("sidebar-bg");
+    });
+
+    //custom scroll bar is only used on desktop
     if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         $(".sidebar-content").mCustomScrollbar({
             axis: "y",
